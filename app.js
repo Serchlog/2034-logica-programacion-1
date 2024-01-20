81cb36ede6e206c5c0adbcf6fcbc24454c7402d5
serch.log <serch.log07@gmail.com>
2024-01-20 10:38:04 -0600

    Solucion

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 //Variables
 //let numeroSecreto = 6;
-let numeroSecreto = Math.floor(Math.random()*10)+1;
+let numeroMaximo = 10;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximo)+1;
 let numeroUsuario = 0;
 let intentos = 1;
 //let palabraveces = 'vez';
 let maximoIntentos = 3;
 
 while(numeroUsuario != numeroSecreto){
-    numeroUsuario = parseInt(prompt("Me indicas un numero entre 1 y 10 por favor:"));
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${numeroMaximo} por favor:`));
 
     console.log(typeof(numeroUsuario));
     /*
