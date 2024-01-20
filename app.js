1e2539b9075c9bf426da9ea7734f3a0018ee6a45
serch.log <serch.log07@gmail.com>
2024-01-20 01:20:09 -0600

    loops

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 //Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraveces = 'vez';
 
-console.log(numeroUsuario);
-/*
-Este código realiza
-la comparación
-*/
-if (numeroUsuario == numeroSecreto) {
-    //Acertaste, fué verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
-    if (numeroUsuario > numeroSecreto) {
-        alert("El número secreto es menor");
+while(numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+
+    console.log(numeroUsuario);
+    /*
+    Este código realiza
+    la comparación
+    */
+    if (numeroUsuario == numeroSecreto) {
+        //Acertaste, fué verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraveces}`);
     } else {
-        alert("El número secreto es mayor")
+        if (numeroUsuario > numeroSecreto) {
+            alert("El número secreto es menor");
+        } else {
+            alert("El número secreto es mayor")
+        }
+        intentos ++;
+        palabraveces = "veces"
+        //La condición no se cumplió
+        //alert("Lo siento, no acertaste el número");
     }
-    //La condición no se cumplió
-    //alert("Lo siento, no acertaste el número");
 }
