7469761d88829177dd6fc0509a444568eb00e2fc
Sergio Ortiz <sortiz079@gmail.com>
2024-01-19 21:07:51 -0600

    Iniciando con JavaScript

---
+++ app_ejercicio1.js
@@ -0,0 +1,24 @@
+//Desafío  1 aler/let/pront
+//1
+alert('¡Bienvenida y bievenido a nuestro sitio web!');
+//2
+let nombre = "Luna";
+//3
+let edad =25;
+//4
+let numeroDeVentas = 50;
+//5
+let saldoDisponible = 1000;
+//6
+alert("¡Error! Completa todos los campos");
+//7
+let mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError);
+//8
+nombre = prompt("Escribe tu nombre:");
+//9
+edad = prompt("Escribe tu edad");
+//10
+if (edad>=10){
+    alert("¡Puedes obtener tu licencia de conducir!");
+}
